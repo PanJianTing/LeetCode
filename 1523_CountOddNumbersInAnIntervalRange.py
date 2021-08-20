33f04bfe1d2d9264ba0d2027d39c4b22aeb8e88d
panjianting <panjt@cathay-ins.com.tw>
2021-08-20 10:10:20 +0800

    1523

---
+++ 1523_CountOddNumbersInAnIntervalRange.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+
+        result = (high - low) // 2
+
+        if (low % 2) == 1 or (high % 2) == 1:
+            result += 1
+        return result
