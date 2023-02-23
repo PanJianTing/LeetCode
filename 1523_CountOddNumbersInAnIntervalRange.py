f9dbdf074b7543c318ebd6c971f9cdaa28ab46cf
panjianting <ab710310@gmail.com>
2023-02-23 11:59:52 +0800

    1523

--- 1523_CountOddNumbersInAnIntervalRange.py
+++ 1523_CountOddNumbersInAnIntervalRange.py
@@ -6,3 +6,21 @@ class Solution:
         if (low % 2) == 1 or (high % 2) == 1:
             result += 1
         return result
+    
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        count = 0
+
+        if low % 2:
+            count += 1
+        elif high % 2:
+            count += 1
+        return (high - low) // 2 + count
+    
+    def countOdds(self, low: int, high: int) -> int:
+
+        return (high - low) // 2 + (low % 2 or high % 2)
+
+    def countOdds(self, low: int, high: int) -> int:
+
+        return ((high + 1) >> 1) - (low >> 1)
