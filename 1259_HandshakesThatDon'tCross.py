b63cb838973c936833da49faf2f482b065391312
panjianting <panjt@cathay-ins.com.tw>
2023-02-28 16:18:23 +0800

    ***1259

---
+++ 1259_HandshakesThatDon'tCross.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfWays(self, numPeople: int) -> int:
+
+        m = 1000000007
+        dp = [0] * (numPeople // 2 + 1)
+        dp[0] = 1
+        for i in range(1, numPeople // 2 + 1):
+            for j in range(i):
+                dp[i] += dp[j] * dp[i-j-1]
+                dp[i] %= m
+        
+        return dp[numPeople//2]
+    
+
+print(Solution().numberOfWays(6))
