f2745a3437b7b8d1cbc8e083a441a7b065994e59
panjianting <ab710310@gmail.com>
2023-03-02 11:45:00 +0800

    422

--- 422_ValidWordSquare.py
+++ 422_ValidWordSquare.py
@@ -9,3 +9,31 @@ class Solution:
 				else:
 					return False
 		return True
+    
+class Solution:
+    def validWordSquare(self, words: list[str]) -> bool:
+
+        afterWords = []
+
+        for i in range(0, len(words)):
+            str = ""
+            for j in range(0, len(words)):
+
+                if i < len(words[j]):
+                    str += words[j][i]
+            
+            afterWords.append(str)
+
+        for i in range(0, len(words)):
+            if words[i] != afterWords[i]:
+                return False
+        return True
+    
+    def validWordSquare(self, words: list[str]) -> bool:
+
+        for i in range(0, len(words)):
+            for j in range(0, len(words[i])):
+                if j >= len(words) or i >= len(words[j]) or words[i][j] != words[j][i]:
+                    return False
+                
+        return True
