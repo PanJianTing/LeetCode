103977107874114ab66964b7a876424ff5308900
panjianting <ab710310@gmail.com>
2023-03-06 11:05:33 +0800

    1539

--- 1539_KthMissingPositiveNumber.py
+++ 1539_KthMissingPositiveNumber.py
@@ -19,7 +19,7 @@ class Solution:
                 right = mid - 1
 
 
-        # 因miss的數在arr[right] ~ arr[left] 之間。
+        # 因miss的數在arr[right] ~ arr[left] 之間。
         # 先找arr[right]之前缺少幾個數 => (arr[right] - (right + 1)) => arr[right] - right - 1 
         # 然後 k 再減去少掉的數 => k - (arr[right] - right -1)
         # 然後在加上 arr[right] => arr[right] + (k - (arr[right] - right - 1))
@@ -62,6 +62,94 @@ class Solution:
 
         return miss
 
-print(Solution.findKthPositive(Solution(), [2,3,4,7,11], 5))
-print(Solution.findKthPositive(Solution(), [1,2,3,4], 2))
+# 2023/03/06 again
+class Solution:
+    def findKthPositive(self, arr: list[int], k: int) -> int:
+
+        left = 0
+        right = len(arr) - 1
+
+        while left < right:
+            mid = (left + right) >> 1
+
+            diff = arr[mid] - (mid + 1)
+
+            if diff < k:
+                left = mid + 1
+            else:
+                right = mid
+
+        diff = k - (arr[left] - left)
+        if diff >= 0:
+            diff += 1
+        
+        return arr[left] + diff
+    
+    # O(n)
+    def findKthPositive(self, arr: list[int], k: int) -> int:
+
+        arrIndex = 0
+        now = 1
+        miss = []
+
+        while k > 0:
+
+            if arrIndex < len(arr):
+
+                if arr[arrIndex] == now:
+                    arrIndex += 1
+                    now += 1
+                    continue
+
+            miss.append(now)
+            now += 1
+            k -= 1
+
+        return miss[-1]
+    
+
+    # O(n)
+    def findKthPositive(self, arr: list[int], k: int) -> int:
+        
+        if k < arr[0]:
+            return k
+        k -= arr[0] - 1
+
+        for i in range(1, len(arr)):
+            miss = arr[i] - (arr[i-1] + 1)
+
+            if miss < k:
+                k -= miss
+            else:
+                return arr[i-1] + k
+            
+        return arr[-1] + k
+    
+
+    def findKthPositive(self, arr: list[int], k: int) -> int:
+
+        left = 0
+        right = len(arr) - 1
+
+        while left <= right:
+            pivot = left + ((right - left) >> 1)
+
+            if arr[pivot] - pivot - 1 < k:
+                left = pivot + 1
+            else:
+                right = pivot - 1
+
+
+        # arr[right] + (k - (arr[right] - (right+1)))
+        # = arr[right] + k - arr[right] + right + 1
+        # = k + (right + 1)
+        # = k + left (because right = left + 1)
+
+        return k + left
+
+print(Solution().findKthPositive([2,3,4,7,11], 5))  #9
+print(Solution().findKthPositive([1,2,3,4], 2))     #6
+print(Solution().findKthPositive([3,4,5,6,7], 2))   #2
+print(Solution().findKthPositive([5,6,7,8,9], 9))   #14
+print(Solution().findKthPositive([1,2], 1))         #3
 print("Hello World!")
