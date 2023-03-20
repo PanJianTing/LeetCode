afc9ae96b3fafc97ea9976240a48802e571220c2
panjianting <ab710310@gmail.com>
2023-03-20 15:00:03 +0800

    605

--- 605_CanPlaceFlowers.py
+++ 605_CanPlaceFlowers.py
@@ -33,5 +33,45 @@ class Solution:
         if n > canAdd:
             return False
         return True
+    
 
-print("Hello World!")
+class Solution:
+    def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
+
+        m = len(flowerbed)
+
+        for i, plot in enumerate(flowerbed):
+            if plot == 0:
+                isCan = True
+                if i-1 > -1:
+                    if flowerbed[i-1] == 1:
+                        isCan = False
+                if i+1 < m:
+                    if flowerbed[i+1] == 1:
+                        isCan = False
+                if isCan:
+                    flowerbed[i] = 1
+                    n -= 1
+        if n > 0:
+            return False
+        return True
+    
+    def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
+
+        m = len(flowerbed)
+        plots = 0
+
+        for i, plot in enumerate(flowerbed):
+
+            if plot == 0:
+                left = (i == 0) or (flowerbed[i-1] == 0)
+                right = (i == m-1) or (flowerbed[i+1] == 0)
+
+                if left and right:
+                    flowerbed[i] = 1
+                    plots += 1
+
+                    if plots >= n:
+                        return True
+                    
+        return plots >= n
