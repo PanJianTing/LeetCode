59fa5e59a6b198dc2103a0972ae644238a4817fa
panjianting <ab710310@gmail.com>
2023-05-04 13:43:49 +0800

    1822

--- 1822_SignOfTheProductOfAnArray.py
+++ 1822_SignOfTheProductOfAnArray.py
@@ -23,3 +23,33 @@ class Solution:
 			return 1
 
 		return -1
+	
+
+class Solution:
+	def arraySign(self, nums: list[int]) -> int:
+		c = 0
+		for num in nums:
+			if num == 0:
+				return 0
+			if num < 0:
+				c += 1
+		return -1 if c % 2 else 1
+	
+	def arraySign(self, nums: list[int]) -> int:
+		c = 1
+		for num in nums:
+			if num == 0:
+				return 0
+			if num < 0:
+				c *= -1
+		return c
+	
+	def arraySign(self, nums: list[int]) -> int:
+		res = 1
+		for num in nums:
+			res *= num
+		if res < 0:
+			return -1
+		elif res > 0:
+			return 1
+		return 0
