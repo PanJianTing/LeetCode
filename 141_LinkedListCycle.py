25187b13e1db7de1bc5df836c2d86bd1149ad2f1
panjianting <ab710310@gmail.com>
2023-09-05 13:38:07 +0800

    141

---
+++ 141_LinkedListCycle.py
@@ -0,0 +1,28 @@
+class ListNode:
+    def __init__(self, x) -> None:
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head):
+        listSet = set()
+
+        while head:
+            if head in listSet:
+                return True
+            listSet.add(head)
+            head = head.next
+        return False
+    
+    def hasCycle(self, head):
+        slow = head
+        fast = head
+
+        while slow and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+            
+        return False
