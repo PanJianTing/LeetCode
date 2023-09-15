7210d66e884a0739d3b36782da11bbcf4c333544
panjianting <ab710310@gmail.com>
2023-09-15 17:18:03 +0800

    332*

--- 332_ReconstructItinerary.py
+++ 332_ReconstructItinerary.py
@@ -1,4 +1,43 @@
 from collections import defaultdict
+import heapq
+
+
+#my first wrong solution
+class Solution:
+    def findItinerary(self, tickets):
+
+        N = len(tickets)
+        pathDic = defaultdict(list)
+        ans = ["JFK"]
+
+
+        for f, t in tickets:
+            temp = pathDic[f]
+            heapq.heappush(temp, t)
+            pathDic[f] = temp
+        
+        def dfs(N, f):
+            if N == 0:
+                return f
+            
+            if pathDic[curr]:
+                return dfs(N-1, heapq.heappop(pathDic[curr]))
+            return ""
+        for _ in range(N):
+            curr = ans[-1]
+            next = heapq.heappop(pathDic[curr])
+            ans.append(next)
+        
+        return ans
+
+
+print(Solution().findItinerary([["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]))
+print(Solution().findItinerary([["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]))
+print(Solution().findItinerary([["JFK","KUL"],["JFK","NRT"],["NRT","JFK"]]))
+
+
+
+
 
 class Solution:
     res = []
