17a8690d8135d49c650d18a0826ffc117866583f
panjianting <ab710310@gmail.com>
2023-09-25 14:40:46 +0800

    389

--- 389_FindTheDifference.py
+++ 389_FindTheDifference.py
@@ -1,4 +1,5 @@
-from collections import Counter
+from collections import Counter, defaultdict
+
 class Solution:
 	def findTheDifference(self, s: str, t: str) -> str:
 
@@ -10,8 +11,7 @@ class Solution:
 		for c in t:
 			diff ^= ord(c)
 
-		return char(diff)
-
+		return chr(diff)
 
 
 
@@ -77,3 +77,63 @@ class Solution:
 
 
 		return difference
+	
+
+class Solution:
+    def findTheDifference(self, s, t):
+        M = len(s)
+        N = len(t)
+
+        s = sorted(s)
+        t = sorted(t)
+
+        ps = 0
+        pt = 0
+        while ps < M and pt < N:
+            if s[ps] == t[pt]:
+                ps += 1
+            else:
+                return t[pt]
+            pt += 1
+        
+        return t[pt]
+    
+    def findTheDifference(self, s: str, t: str) -> str:
+        t_dic = defaultdict(int)
+
+        for c in t:
+            t_dic[c] += 1
+
+        for c in s:
+            t_dic[c] -= 1
+
+        for key, val in t_dic.items():
+            if val:
+                return key
+        return t[0]
+    
+    def findTheDifference(self, s, t):
+        s_dic = defaultdict(int)
+
+        for c in s:
+            s_dic[c] += 1
+        
+        for c in t:
+            s_dic[c] -= 1
+            if s_dic[c] < 0:
+                return c
+            
+    def findTheDifference(self, s, t):
+        ans = 0
+        
+        for c in s:
+            ans ^= ord(c)
+        
+        for c in t:
+            ans ^= ord(c)
+        
+        return chr(ans)
+    
+    
+print(Solution().findTheDifference("abcd", "abcde"))
+print(Solution().findTheDifference("", "y"))
