4721a004ba9c6a387c93a5b3d4c5472b54b6573b
panjianting <ab710310@gmail.com>
2023-10-27 15:16:18 +0800

    5

--- 5_LongestPalindromicSubstring.py
+++ 5_LongestPalindromicSubstring.py
@@ -70,3 +70,127 @@ class Solution:
 				start += 1
 
 		return s
+
+
+# 2023/10/27 second
+
+class Solution:
+	def longestPalindrome(self, s: str) -> str:
+		N = len(s)
+		def helper(i, j):
+			l = i
+			r = j
+
+			while l <= r:
+				if s[l] == s[r]:
+					l += 1
+					r -= 1
+				else:
+					return False
+				
+			return True
+		
+		
+		for length in range(N, 0, -1):
+			for i in range(0, N-length+1):
+				j = i+length-1
+				if helper(i, j):
+					return s[i:j+1]
+		
+		return ""
+	
+	def longestPalindrome(self, s: str) -> str:
+		N = len(s)
+
+		dp = [[False] * N for _ in range(N)]
+
+		ans = [0, 0]
+
+		for i in range(N):
+			dp[i][i] = True
+
+		for i in range(N-1):
+			if s[i] == s[i+1]:
+				dp[i][i+1] = True
+				ans = [i, i+1]
+
+		for diff in range(2, N):
+			for i in range(0, N - diff):
+				j = i + diff
+				if s[i] == s[j] and dp[i+1][j-1]:
+					dp[i][j] = True
+					ans[0] = i
+					ans[1] = j
+		
+		return s[ans[0]:ans[1]+1]
+	
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+        ans = ""
+        N = len(s)
+
+        for i in range(0, N):
+            for j in range(0, i+1):
+                # print(s[j:i+1], s[j:i+1:-1])
+                now = s[j:i+1]
+                # print("".join(reversed(now)))
+                if now == "".join(reversed(now)):
+                    if len(s[j:i+1]) > len(ans):
+                        ans = s[j:i+1]
+
+        return ans
+    
+    def longestPalindrome(self, s: str) -> str:
+        
+        ans = ""
+        N = len(s)
+
+        dp = [[""] * (N+1) for _ in range(N+1)]
+        revers_s = s[::-1]
+
+        for i in range(0, N):
+            for j in range(0, N):
+                if s[i] == revers_s[j]:
+                    dp[i+1][j+1] += dp[i][j] + s[i]
+                    if len(dp[i+1][j+1]) > len(ans):
+                        ans = dp[i+1][j+1]
+        
+        return ans
+    
+    def longestPalindrome(self, s):
+
+        N = len(s)
+
+        def helper(i, j):
+            l = i
+            r = j
+            while l >= 0 and r < N and s[l] == s[r]:
+                l -= 1
+                r += 1
+
+            return r - l - 1
+        
+        ans = [0, 0]
+
+        for i in range(N):
+            oddL = helper(i, i)
+            if oddL > (ans[1] - ans[0] + 1):
+                dist = oddL // 2
+                ans[0] = i-dist
+                ans[1] = i+dist
+            
+            evenL = helper(i, i+1)
+            if evenL > (ans[1] - ans[0] + 1):
+                dist = evenL // 2 - 1
+                ans[0] = i - dist 
+                ans[1] = i + dist + 1
+
+        return s[ans[0]:ans[1]+1]
+		
+
+print(Solution().longestPalindrome("babad"))
+print(Solution().longestPalindrome("cbbd"))
+print(Solution().longestPalindrome("a"))
+
