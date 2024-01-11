f5b096cef49b6e3501a619e4faeb8205952e0782
panjianting <ab710310@gmail.com>
2024-01-11 17:54:04 +0800

    1026

--- 1026_MaximumDifferenceBetweenNodeAndAncestor.py
+++ 1026_MaximumDifferenceBetweenNodeAndAncestor.py
@@ -8,6 +8,23 @@ class TreeNode:
 class Solution:
 
     def maxAncestorDiff(self, root) -> int:
+        self.res = 0
+        if root == None:
+            return self.res
+
+        def helper(cur, min_val, max_val):
+            if cur == None:
+                return
+            self.res = max(self.res, abs(cur.val - max_val), abs(cur.val - min_val))
+            min_val = min(cur.val, min_val)
+            max_val = max(cur.val, max_val)
+            helper(cur.left, min_val, max_val)
+            helper(cur.right, min_val, max_val)
+            return
+        helper(root, root.val, root.val)
+        return self.res
+
+    def maxAncestorDiff(self, root) -> int:
         if root == None:
             return 0
         
