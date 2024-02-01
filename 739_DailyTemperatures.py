4e9329d99b174724bb6c9143032b09a52591b2c8
panjianting <ab710310@gmail.com>
2024-02-01 20:20:48 +0800

    739

--- 739_DailyTemperatures.py
+++ 739_DailyTemperatures.py
@@ -34,6 +34,41 @@ class Solution:
             st.append(i)
         return ans
     
+    def dailyTemperatures(self, t: list[int]) -> list[int]:
+        N = len(t)
+        ans = [0] * N
+        hottest = 0
+
+        for i in range(N-1, -1, -1):
+            if t[i] >= hottest:
+                hottest = t[i]
+                continue
+            
+            curIdx = i+1
+            while t[i] >= t[curIdx]:
+                curIdx += ans[curIdx]
+            ans[i] = curIdx - i
+        
+        return ans
+    
+    def dailyTemperatures(self, t: list[int]) -> list[int]:
+        N = len(t)
+        ans = [0] * N
+        hottest = 0
+
+        for curDay in range(N-1, -1, -1):
+            if t[curDay] >= hottest:
+                hottest = t[curDay]
+                continue
+            
+            day = 1
+            while t[curDay + day] <= t[curDay]:
+                day += ans[curDay + day]
+            ans[curDay] = day
+        
+        return ans
+    
+print(Solution().dailyTemperatures([34,80,80,34,34,80,80,80,80,34]))
 print(Solution().dailyTemperatures([73,74,75,71,69,72,76,73]))
 print(Solution().dailyTemperatures([30,40,50,60]))
 print(Solution().dailyTemperatures([30,60,90]))
