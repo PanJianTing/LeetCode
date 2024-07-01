63b6f2327412c37e69293582daeccdbd2ae2cd3d
panjianting <ab710310@gmail.com>
2024-07-01 11:20:49 +0800

    1550

--- 1550_ThreeConsecutiveOdds.py
+++ 1550_ThreeConsecutiveOdds.py
@@ -12,3 +12,35 @@ class Solution:
                 count += 1
 
         return False
+    
+    def threeConsecutiveOdds(self, arr: list[int]) -> bool:
+        N = len(arr)
+
+        for i in range(N-2):
+            if 1 == (arr[i] & 1) == (arr[i+1] & 1) == (arr[i+2] & 1):
+                return True
+        
+        return False
+
+    def threeConsecutiveOdds(self, arr: list[int]) -> bool:
+        N = len(arr)
+        cnt = 0
+
+        for i in range(N):
+            if arr[i] & 1:
+                cnt += 1
+            else:
+                cnt = 0
+            if cnt == 3:
+                return True
+        
+        return False
+
+    def threeConsecutiveOdds(self, arr: list[int]) -> bool:
+        N = len(arr)
+
+        for i in range(N-2):
+            if (arr[i] * arr[i+1] * arr[i+2]) & 1:
+                return True
+        
+        return False
