fc4d057e4cb0dc758075cd0b6cb7be1d20826739
panjianting <ab710310@gmail.com>
2024-07-07 10:19:16 +0800

    1518

--- 1518_WaterBottles.py
+++ 1518_WaterBottles.py
@@ -1,12 +1,37 @@
 class Solution:
-	def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-		
-		drink = numBottles
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        total = 0
+        cur_drink = numBottles
+        remaining_bottles = 0
 
-		while numBottles >= numExchange:
+        while cur_drink > 0:
+            total += cur_drink
+            remaining_bottles += cur_drink
+            next_drink = remaining_bottles // numExchange
+            remaining_bottles %= numExchange
+            cur_drink = next_drink
 
-			drink += numBottles // numExchange
-			numBottles = (numBottles // numExchange) + numBottles % numExchange
+        return total
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        drink = 0
 
-		return drink
-		
+        while numBottles >= numExchange:
+
+            numBottles = numBottles - numExchange + 1
+            drink += numExchange
+        
+        return drink + numBottles
+    
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        drink = 0
+        
+        while numBottles >= numExchange:
+            can_drink = numBottles // numExchange
+            numBottles = numBottles - can_drink * numExchange + can_drink
+            drink += can_drink * numExchange
+
+        return drink + numBottles
+
+
+print(Solution().numWaterBottles(9, 3))
+print(Solution().numWaterBottles(15, 4))
