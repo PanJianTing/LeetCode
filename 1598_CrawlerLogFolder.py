f602b518f80eb047f6c2cc5d03c1a0893ebe1c6c
panjianting <ab710310@gmail.com>
2024-07-10 09:20:28 +0800

    1598

--- 1598_CrawlerLogFolder.py
+++ 1598_CrawlerLogFolder.py
@@ -1,17 +1,32 @@
-class  Solution:
-	def minOperations(self, logs: list[str]) -> int:
+class Solution:
+    def minOperations(self, logs: list[str]) -> int:
+        N = len(logs)
+        res = 0
 
-		main = 0
+        for i in range(N):
+            cur = logs[i]
+            if '../' == cur:
+                if res:
+                    res -= 1
+            elif './' == cur:
+                continue
+            else:
+                res += 1
+        return res
+    
+    def minOperations(self, logs: list[str]) -> int:
+        st = []
 
-		for log in logs:
-			if log == '../':
-				if main == 0:
-					main = 0
-				else:
-					main -= 1
-			elif log == './':
-				pass
-			else:
-				main += 1
-
-		return main
+        for log in logs:
+            if '../' == log:
+                if st:
+                    st.pop()
+            elif './' == log:
+                continue
+            else:
+                st.append(log)
+        
+        return len(st)
+    
+print(Solution().minOperations(["d1/","d2/","../","d21/","./"]))
+        
