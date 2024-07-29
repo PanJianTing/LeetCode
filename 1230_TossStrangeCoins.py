149b1fcbc66a3dc0a69cd55d520b354544db48aa
panjianting <ab710310@gmail.com>
2024-07-29 16:42:26 +0800

    1230

--- 1230_TossStrangeCoins.py
+++ 1230_TossStrangeCoins.py
@@ -1,20 +1,51 @@
 from functools import cache
 
 class Solution:
-    def probabilityOfHeads(self, A, target) -> float:        
-        N = len(A)
-
+    def probabilityOfHeads(self, prob: list[float], target: int) -> float:
+        N = len(prob)
+        
         @cache
-        def dp(idx, target):
-            if target < 0:
+        def dp(idx, cnt):
+            
+            if cnt > target:
                 return 0
             
             if idx == N:
-                if target == 0:
+                if target == cnt:
                     return 1
-                else:
-                    return 0
-            
-            return dp(idx + 1, target-1) * A[idx] + dp(idx + 1, target) * (1 - A[idx])
+                return 0
+
+            return dp(idx+1, cnt+1) * prob[idx] + dp(idx+1, cnt) * (1-prob[idx])
+
+        return dp(0, 0)
+    
+    def probabilityOfHeads(self, prob: list[float], target: int) -> float:
+        N = len(prob)
+        dp = [[0.0] * (target+1) for _ in range(N+1)]
+        dp[0][0] = 1.0
+
+        for i in range(1, N+1):
+            dp[i][0] = dp[i-1][0] * (1 - prob[i-1])
+            for j in range(1, target+1):
+                dp[i][j] = dp[i-1][j-1] * prob[i-1] + dp[i-1][j] * (1- prob[i-1])
+
         
-        return dp(0, target)
+        return dp[N][target]
+    
+    def probabilityOfHeads(self, prob: list[float], target: int) -> float:
+        N = len(prob)
+        dp = [0.0] * (target + 1)
+        dp[0] = 1
+        
+        for i in range(1, N+1):
+            for j in range(target, 0, -1):
+                dp[j] = dp[j-1] * prob[i-1] + dp[j] * (1 - prob[i-1])
+            dp[0] = dp[0] * (1-prob[i-1])
+
+        return dp[target]
+    
+
+
+print(Solution().probabilityOfHeads([0.2,0.8,0,0.3,0.5], 3))
+print(Solution().probabilityOfHeads([0.4], 1))
+print(Solution().probabilityOfHeads([0.5,0.5,0.5,0.5,0.5], 0))
