597f595d79d6f612b2a0eda9be56e2a8d7e3f55f
panjianting <ab710310@gmail.com>
2024-08-17 14:53:00 +0800

    860

--- 860_LemonadeChange.py
+++ 860_LemonadeChange.py
@@ -1,54 +1,46 @@
 class Solution:
     def lemonadeChange(self, bills: list[int]) -> bool:
-        five = ten = 0
-        
-        for bill in bills:
-            if bill == 5:
-                five += 1
-            elif bill == 10:
-                if five == 0:
+        bill_map = {5:0, 10: 0}
+
+        for b in bills:
+            if b == 5:
+                bill_map[5] += 1
+            elif b == 10:
+                if bill_map[5] > 0:
+                    bill_map[5] -= 1
+                    bill_map[10] += 1
+                else:
                     return False
-                five -= 1
-                ten += 1
             else:
-                if ten > 0 and five > 0:
-                    ten -= 1
-                    five -= 1
-                elif five >= 3:
-                    five -= 3
+                if bill_map[10] > 0 and bill_map[5] > 0:
+                    bill_map[10] -= 1
+                    bill_map[5] -= 1
+                elif bill_map[5] >= 3:
+                    bill_map[5] -= 3
                 else:
                     return False
         return True
     
-    def lemonadeChange_my(self, bills: list[int]) -> bool:
-        nowBills = {
-            5:0,
-            10:0
-        }
+    def lemonadeChange(self, bills: list[int]) -> bool:
+        five, ten = 0, 0
         
-        for bill in bills:
-            if bill == 5:
-                nowBills[bill] += 1
-            elif bill == 10:
-                if nowBills[5] == 0:
-                    return False
+        for b in bills:
+            if b == 5:
+                five += 1
+            elif b == 10:
+                five -= 1
+                ten += 1
+            else:
+                if ten > 0:
+                    ten -= 1
+                    five -= 1
                 else:
-                    nowBills[5] -= 1
-                    nowBills[10] += 1
-            elif bill == 20:
-                if nowBills[10] > 0:
-                    if nowBills[5] == 0:
-                        return False:
-                    else:
-                        nowBills[10] -= 1
-                        nowBills[5] -= 1
-                else:
-                    if nowBills[5] < 3:
-                        return False:
-                    else:
-                        nowBills[5] -= 3
-
+                    five -= 3
+            if five < 0:
+                return False
         return True
-                
+    
+print(Solution().lemonadeChange([5,5,5,10,20]))
+print(Solution().lemonadeChange([5,5,10,10,20]))
 
-print("Hello World!")
+        
