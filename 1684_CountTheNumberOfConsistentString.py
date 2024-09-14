c4c2fbfa6ac088818aea80d2992a68633226d276
panjianting <ab710310@gmail.com>
2024-09-14 14:22:37 +0800

    1684

--- 1684_CountTheNumberOfConsistentString.py
+++ 1684_CountTheNumberOfConsistentString.py
@@ -12,6 +12,66 @@ class Solution:
                 res += 1
         return res
     
+    def countConsistentStrings(self, allowed: str, words: list[str]) -> int:
+        res = 0
+        
+        for w in words:
+            is_allow = True
+
+            for c in w:
+                is_char_allow = False
+
+                for a in allowed:
+                    if c == a:
+                        is_char_allow = True
+                        break
+                if is_char_allow == False:
+                    is_allow = False
+                    break
+            
+            if is_allow:
+                res += 1
+        return res
+    
+
+    def countConsistentStrings(self, allowed: str, words: list[str]) -> int:
+        res = 0
+        allow_list = [False] * 26
+
+        for a in allowed:
+            allow_list[ord(a) - ord('a')] = True
+
+        for w in words:
+            is_allow = True
+
+            for c in w:
+                if allow_list[ord(c) - ord('a')] == False:
+                    is_allow = False
+                    break
+            if is_allow:
+                res += 1
+        return res
+    
+
+    def countConsistentStrings(self, allowed: str, words: list[str]) -> int:
+        res = 0
+        allow_set = set()
+
+        for a in allowed:
+            allow_set.add(a)
+
+        for w in words:
+            is_allow = True
+
+            for c in w:
+                if c not in allow_set:
+                    is_allow = False
+                    break
+            if is_allow:
+                res += 1
+        return res
+
+    
 
 print(Solution().countConsistentStrings("ab", ["ad","bd","aaab","baa","badab"]))
 print(Solution().countConsistentStrings("abc", ["a","b","c","ab","ac","bc","abc"]))
