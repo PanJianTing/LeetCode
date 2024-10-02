c13a96b6759de3072650fe555860922aa1879cfd
panjianting <ab710310@gmail.com>
2024-10-02 11:43:35 +0800

    1331

--- 1331_RankTransformOfAnArray.py
+++ 1331_RankTransformOfAnArray.py
@@ -15,3 +15,59 @@ class Solution:
 
 
         return rank
+    
+    def arrayRankTransform(self, arr: list[int]) -> list[int]:
+        N = len(arr)
+        sort_array = sorted(arr)
+        res = []
+        rank_map = {}
+        rank = 1
+
+        for n in sort_array:
+            if n not in rank_map:
+                rank_map[n] = rank
+                rank += 1
+        
+        for n in arr:
+            res.append(rank_map[n])
+
+        return res
+    
+
+    def arrayRankTransform(self, arr: list[int]) -> list[int]:
+        N = len(arr)
+        res = []
+        rank_map = {}
+        sort_arr = sorted(arr)
+        rank = 1
+        
+        for i in range(N):
+            if i > 0 and sort_arr[i] > sort_arr[i-1]:
+                rank += 1
+            rank_map[sort_arr[i]] = rank
+
+        for n in arr:
+            res.append(rank_map[n])
+
+        return res
+    
+    def arrayRankTransform(self, arr: list[int]) -> list[int]:
+        N = len(arr)
+        sort_arr = sorted(set(arr))
+        rank = 1
+        rank_map = {}
+        res = []
+
+        for n in sort_arr:
+            rank_map[n] = rank
+            rank += 1
+        
+        for n in arr:
+            res.append(rank_map[n])
+
+        return res
+
+
+
+    
+print(Solution().arrayRankTransform([40,10,20,30]))
