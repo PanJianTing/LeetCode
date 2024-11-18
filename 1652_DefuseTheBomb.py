393408571d0de70fea576ed3242c394572ae77d2
panjianting <ab710310@gmail.com>
2024-11-18 10:47:27 +0800

    1652

--- 1652_DefuseTheBomb.py
+++ 1652_DefuseTheBomb.py
@@ -1,46 +1,76 @@
-
 class Solution:
     def decrypt(self, code: list[int], k: int) -> list[int]:
+        N = len(code)
         if k == 0:
-            return [0] * len(code)
+            return [0] * N
+
+        ans = []
+        check_code = list(code)
         if k < 0:
-            return list(reversed(self.decrypt(list(reversed(code)), -k)))
-        # brute force
-        res = [0] * len(code)
-        code2 = code * 2
-        # for i in range(len(code)):
-        #     res[i] = sum(code2[i+1:i+1+k])
-        #
-        # accumulative sum
-        sums = [0] * len(code2)
-        sums[0] = code[0]
-        for i in range(1, len(code2)):
-            sums[i] = sums[i-1] + code2[i]
-        for i in range(len(code)):
-            res[i] = sums[i+k] - sums[i]
-        return res
-
-
-    def decrypt_my(self, code: list[int], k: int) -> list[int]:
-
-        length = len(code)
-        result = []
-        if k == 0:
-            return [0] * length
-
-        elif k > 0:
-
-            for i in range(length):
-                sum = 0
-                for j in range(1, k + 1):
-                    sum += code[((i + j) % length)]
-                result.append(sum)
-
-            return result
+            check_code = code[::-1]
         
+        for i in range(N):
+            cur_sum = 0
+            for j in range(i+1, i+abs(k)+1):
+                cur_sum += check_code[j % N]
+            ans.append(cur_sum)
+
+        return ans if k > 0 else ans[::-1]
+    
+
+    def decrypt(self, code: list[int], k: int) -> list[int]:
+        N = len(code)
+
+        if k == 0:
+            return [0] * N
+        
+        ans = []
+
+        for i in range(N):
+            cur_sum = 0
+            if k > 0:
+                for j in range(i+1, i+k+1):
+                    cur_sum += code[j % N]
+            else:
+                for j in range(i-(-k), i):
+                    cur_sum += code[j]
+            ans.append(cur_sum)
+        return ans
+    
+
+    def decrypt(self, code: list[int], k: int) -> list[int]:
+        N = len(code)
+
+        if k == 0:
+            return [0] * N
+        ans = []
+        cur_sum = 0
+        st = 0
+        end = 0
+
+        if k > 0:
+            st = 1
+            for i in range(1, k+1):
+                cur_sum += code[i]
+                end = i
         else:
+            st = N-(-k)
+            for i in range(N - (-k), N):
+                cur_sum += code[i]
+                end = i
 
-            return self.decrypt(code[::-1], -1*k)[::-1]
+        ans.append(cur_sum)
 
+        for i in range(1, N):
+            cur_sum -= code[st % N]
+            cur_sum += code[(end + 1) % N]
+            st += 1
+            end += 1
+            ans.append(cur_sum)
+        
+        return ans
+    
+# print(Solution().decrypt([5,7,1,4], 3))
+# print(Solution().decrypt([2,4,9,3], -2))
+print(Solution().decrypt([10,5,7,7,3,2,10,3,6,9,1,6], -4))
 
-Solution.decrypt(Solution(), [5,7,1,4], 3)
