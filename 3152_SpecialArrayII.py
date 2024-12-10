7cc4227645ebe46c4aef0aba50e328ba22ddbb62
panjianting <ab710310@gmail.com>
2024-12-10 10:12:23 +0800

    3152

--- 3152_SpecialArrayII.py
+++ 3152_SpecialArrayII.py
@@ -1,40 +1,40 @@
 class Solution:
     def isArraySpecial(self, nums: list[int], queries: list[list[int]]) -> list[bool]:
-        res = []
+        no_idx = []
+        ans = []
 
-        for q in queries:
-            q_res = True
-            for i in range(q[0], q[1]):
-                if nums[i] % 2 == nums[i+1] % 2:
-                    q_res = False
-                    break
-            res.append(q_res)
-            
-        return res
-    
-    def isArraySpecial(self, nums: list[int], queries: list[list[int]]) -> list[bool]:
-        N = len(nums)
-        valid_pairs = [1] * (N-1)
-        prefix_pairs = [0] * N
-        res = []
+        for i in range(1, len(nums)):
+            if nums[i-1] & 1 == nums[i] & 1:
+                no_idx.append(i)
 
-        for i in range(N-1):
-            if nums[i] % 2 == nums[i+1] % 2:
-                valid_pairs[i] = 0
-            else:
-                valid_pairs[i] = 1
-        
-        for i in range(1, N):
-            prefix_pairs[i] = prefix_pairs[i-1] + valid_pairs[i-1]
-        
+        def bs(st, end):
+            l = 0
+            r = len(no_idx) - 1
+
+            while l <= r:
+                m = l + ((r-l) >> 1)
+
+                if no_idx[m] < st:
+                    l = m + 1
+                elif no_idx[m] > end:
+                    r = m - 1
+                else:
+                    return True
+            return False
+
         for st, end in queries:
-            if prefix_pairs[end] - prefix_pairs[st] == end - st:
-                res.append(True)
-            else:
-                res.append(False)
-        return res
-        
 
+            if bs(st+1, end):
+                ans.append(False)
+            else:
+                ans.append(True)
+        return ans
+    
 
 print(Solution().isArraySpecial([3,4,1,2,6], [[0,4]]))
 print(Solution().isArraySpecial([4,3,1,6], [[0,2],[2,3]]))
+
+
+
+
+            
