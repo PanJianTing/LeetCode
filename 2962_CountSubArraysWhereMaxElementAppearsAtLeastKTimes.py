4a7bd67631bb7e8ba17787d16d7b95b081dc5d34
panjianting <ab710310@gmail.com>
2025-05-05 14:09:45 +0800

    2962

--- 2962_CountSubArraysWhereMaxElementAppearsAtLeastKTimes.py
+++ 2962_CountSubArraysWhereMaxElementAppearsAtLeastKTimes.py
@@ -1,5 +1,5 @@
 class Solution:
-    def countSubarray(self, nums: list[int], k: int) -> int:
+    def countSubarrays(self, nums: list[int], k: int) -> int:
         N = len(nums)
         max_num = max(nums)
         cur_cnt = 0
@@ -17,8 +17,8 @@ class Solution:
         
         return res
     
-print(Solution().countSubarray([1,3,2,3,3], 2))
+print(Solution().countSubarrays([1,3,2,3,3], 2))
 
-print(Solution().countSubarray([4,3,7,10,2,10,1,6,10,7,10,10,9,8,3], 3))
+print(Solution().countSubarrays([4,3,7,10,2,10,1,6,10,7,10,10,9,8,3], 3))
 
         
