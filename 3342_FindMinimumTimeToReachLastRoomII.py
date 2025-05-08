a4ba3e2f9102bdbc26349fc7b94585134ebdbdd8
panjianting <ab710310@gmail.com>
2025-05-08 14:22:22 +0800

    3342

--- 3342_FindMinimumTimeToReachLastRoomII.py
+++ 3342_FindMinimumTimeToReachLastRoomII.py
@@ -2,29 +2,30 @@ import heapq
 
 class Solution:
     def minTimeToReach(self, moveTime: list[list[int]]) -> int:
-        M = len(moveTime)
-        N = len(moveTime[0])
+        ROW = len(moveTime)
+        COL = len(moveTime[0])
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        visit = set()
 
         hq = []
-        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        visited = [[False] * N for _ in range(M)]
-        heapq.heappush(hq, [0, 0, 0, 1])
-        visited[0][0] = True
+        heapq.heappush(hq, (0, 0, 0, 1))
+        visit.add((0, 0))
 
         while hq:
-            cost, cur_i, cur_j, cur_cost = heapq.heappop(hq)
+            cur_t, cur_r, cur_c, cur_move_time = heapq.heappop(hq)
 
-            if cur_i == M-1 and cur_j == N-1:
-                return cost
-            
-            for di, dj in dirs:
-                next_i = di + cur_i
-                next_j = dj + cur_j
+            if cur_r == ROW - 1 and cur_c == COL - 1:
+                return cur_t
+            next_move_time = 2 if cur_move_time == 1 else 1
+            for dr, dc in dirs:
+                next_r = cur_r + dr
+                next_c = cur_c + dc
+                if 0 <= next_r < ROW and 0 <= next_c < COL:
+                    if (next_r, next_c) not in visit:
+                        next_t = max(cur_t, moveTime[next_r][next_c]) + cur_move_time
+                        visit.add((next_r, next_c))
+                        heapq.heappush(hq, (next_t, next_r, next_c, next_move_time))
 
-                if 0 <= next_i < M and 0 <= next_j < N and visited[next_i][next_j] == False:
-                    visited[next_i][next_j] = True
-                    heapq.heappush(hq, [max(cost + cur_cost, moveTime[next_i][next_j] + cur_cost), next_i, next_j, 1 if cur_cost == 2 else 2])
-        
         return -1
 
                 
