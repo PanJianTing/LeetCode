6c91573453742690d87827f134b2a316aecf4d42
panjianting <ab710310@gmail.com>
2025-05-26 16:02:10 +0800

    1857

--- 1857_LargestColorValueInADirectedGraph.py
+++ 1857_LargestColorValueInADirectedGraph.py
@@ -1,48 +1,41 @@
-from collections import deque
 from collections import defaultdict
+from collections import deque
 
-class Solution:    
+class Solution:
     def largestPathValue(self, colors: str, edges: list[list[int]]) -> int:
-
-        n = len(colors)
-        adj = defaultdict(list)
-        indegree = [0] * n
+        N = len(colors)
+        adj_map = defaultdict(list)
+        indegree = [0] * N
 
         for u, v in edges:
-            adj[u].append(v)
+            adj_map[u].append(v)
             indegree[v] += 1
 
-        count = [[0 for _  in range(26)] for _ in range(n)]
+        count = [[0] * 26 for _ in range(N)]
         q = deque()
 
-        for i in range(n):
+        for i in range(N):
             if indegree[i] == 0:
                 q.append(i)
-
-        ans = 1
-        seen = 0
-        while len(q):
-            curr = q.popleft()
-            currColor = colors[curr]
-            count[curr][ord(currColor) - ord('a')] += 1
-            ans = max(ans, count[curr][ord(currColor) - ord('a')])
-
-            seen += 1
-
-            if len(adj[curr]) == 0:
-                continue
-
-            for neighbor in adj[curr]:
-                for i in range(26):
-                    count[neighbor][i] = max(count[neighbor][i], count[curr][i])
-
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    q.append(neighbor)
-
-        if seen < n:
-            return -1
         
-        return ans
+        ans = 1
+        visit_node = 0
 
-            
+        while q:
+            cur_node = q.popleft()
+            cur_color = ord(colors[cur_node]) - ord('a')
+            count[cur_node][cur_color] += 1
+            ans = max(ans, count[cur_node][cur_color])
+            visit_node += 1
+
+            for nei in adj_map[cur_node]:
+                for i in range(26):
+                    count[nei][i] = max(count[cur_node][i], count[nei][i])
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+
+        return -1 if visit_node < N else ans
+    
+
+print(Solution().largestPathValue("abaca", [[0,1],[0,2],[2,3],[3,4]]))
